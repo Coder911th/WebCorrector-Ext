97d6b7fc955c38a9a5fff9157f7715728d539220
Dmitry Lozhkin <31573763+Coder911th@users.noreply.github.com>
2018-06-21 22:41:12 +0300

    Fix FormItem component

--- client/src/components/base/FormItem.vue
+++ client/src/components/base/FormItem.vue
@@ -1,70 +1,66 @@
 <!--
     Элемент формы, состоящий из метки и произвольного содержимого
     В слоте по умолчанию передаётся элемент формы
+    В слоте label передаётся содержимое метки
 -->
 <template>
-    <div class="form-item">
-        <div 
-                class="form-item__label"
-                :style="{
-                    width,
-                    textAlign: align
-                }"
-                @click="activeLink">
-            <slot name="label"/>
-        </div>
-        <div ref="control" class="form-item__control">
-            <slot/>
-        </div>
+  <div class="form-item">
+    <div 
+        class="form-item__label"
+        :style="{
+          width,
+          textAlign: align
+        }"
+        @click="activeLink">
+      <slot name="label"/>
     </div>
+    <div ref="control" class="form-item__control">
+      <slot/>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'FormItem',
-    props: {
-        // Метка элемента формы
-        label: {
-            type: String,
-            default: ''
-        },
-        // Ширина, которая отводится под label
-        width: {
-            type: Number,
-            default: null
-        },
-        // Выравнивание метки
-        align: {
-            type: String,
-            default: 'left'
-        }
+  name: 'FormItem',
+  props: {
+    // Ширина, которая отводится под label
+    width: {
+      type: Number,
+      default: null
     },
-    methods: {
-        activeLink() {
-            let control = this.$refs.control.firstElementChild;
-            if (control) {console.log(control)
-                control.focus();
-            }
-        }
+    // Выравнивание метки
+    align: {
+      type: String,
+      default: 'left'
     }
+  },
+  methods: {
+    activeLink() {
+      let control = this.$refs.control.firstElementChild;
+      if (control) {
+        control.focus();
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .form-item {
-    display: flex;
+  display: flex;
 }
 
 .form-item__label {
-    margin-right: 5px;
-    cursor: pointer;
-    user-select: none;
-    &:hover {
-        color: #555;
-    }
+  margin-right: 5px;
+  cursor: pointer;
+  user-select: none;
+  &:hover {
+    color: #555;
+  }
 }
 
 .form-item__control {
-    flex-grow: 1;
+  flex-grow: 1;
 }
 </style>
